4dd2b51184eb86031f28b7db4b7680ada908d9da
izayoi5776 <izayoi5776@gmail.com>
2023-09-18 11:34:21 +0900

    显示时边线标记垂直水平位置

--- src/main/java/com/example/demo/Config.java
+++ src/main/java/com/example/demo/Config.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 public class Config {
+	static final int KEY_ESC = 27;
 	static final int KEY_UP = 57416;
 	static final int KEY_DOWN = 57424;
 	static final int KEY_LEFT = 57419;
@@ -9,4 +10,43 @@ public class Config {
 	static final int KEY_CTRL_DOWN = 57489;
 	static final int KEY_CTRL_LEFT = 57459;
 	static final int KEY_CTRL_RIGHT = 57460;
+	
+	
+	static String key2Str(int key){
+		String s = "";
+		switch(key){
+		case KEY_UP:
+			s = "UP";
+			break;
+		case KEY_DOWN:
+			s = "DOWN";
+			break;
+		case KEY_LEFT:
+			s = "LEFT";
+			break;
+		case KEY_RIGHT:
+			s = "RIGHT";
+			break;
+		case KEY_CTRL_UP:
+			s = "CTRL_UP";
+			break;
+		case KEY_CTRL_DOWN:
+			s = "CTRL_DOWN";
+			break;
+		case KEY_CTRL_LEFT:
+			s = "CTRL_LEFT";
+			break;
+		case KEY_CTRL_RIGHT:
+			s = "CTRL_RIGHT";
+			break;
+		case KEY_ESC:
+			s = "ESC";
+			break;
+		default:
+			s = "UNKNOWN";
+			break;
+		}
+		s += "(" + key + ")";
+		return s;
+	}
 }
